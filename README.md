d178f576a7f7745a776793895fcff63ca8067e63
Fidek-creator <fidek1999@gmail.com>
2022-12-28 15:00:18 +0100

    intial project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Interactive-room
+# vue-v3d-app
+
+## How to run the application
+Move into the project's root directory, then run the following command to
+install necessary dependencies:
+
+```
+npm install
+```
+
+After that start the development server with:
+
+```
+npm run serve
+```
+
+By default the application now should be available at <http://localhost:8080/>.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/AppsGrid.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="apps-container">
+    <V3DApp v-if="showTL"/><div v-else></div>
+  </div>
+  <div class="control-buttons">
+    <button class="control-buttons__button" @click="showTL = !showTL">{{tlLabel}}</button>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+import V3DApp from './components/V3DApp.vue';
+
+export default {
+  name: 'AppsGrid',
+
+  components: {
+    V3DApp,
+  },
+
+  setup() {
+    const showTL = ref(true);
+    const showTR = ref(true);
+    const showBL = ref(true);
+    const showBR = ref(true);
+
+    const tlLabel = computed(() => `${showTL.value ? 'Hide' : 'Show'} Top Left`);
+    const trLabel = computed(() => `${showTR.value ? 'Hide' : 'Show'} Top Right`);
+    const blLabel = computed(() => `${showBL.value ? 'Hide' : 'Show'} Bottom Left`);
+    const brLabel = computed(() => `${showBR.value ? 'Hide' : 'Show'} Bottom Right`);
+
+    return {
+      showTL, showTR, showBL, showBR,
+      tlLabel, trLabel, blLabel, brLabel,
+    };
+  },
+}
+</script>
+
+<style>
+@import './assets/css/main.css';
+</style>

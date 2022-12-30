18001ee5e8da7126952f7cc35bd409aac04dc6c3
Fidek-creator <fidek1999@gmail.com>
2022-12-30 22:54:29 +0100

    Rooms and Furnitures

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,42 +1,57 @@
 <script setup>
 import { list } from 'postcss';
 import { ref } from 'vue'
+
 const rooms= ref([
   "Kitchen",
 ])
 </script>
 
 <template>
-<section class="bg-slate-400">
-  <p class="text-sm text-center font-medium p-1">Aplikacja wspierająca pracę projektantów</p>
-  <hr class="h-px border-0 bg-orange-500 my-1">
-  
-  <div class="bg-cyan-700 text-center flex items-center h-8 mx-4 my-3">
-    <span class="inline mx-8 my-8">
-      <svg class="h-9 w-9  fill-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-      </svg>
-    </span>
-  
-    Kuchnia 
+   <div class="py-4 px-3 bg-slate-700 rounded flex flex-col">
     
-  </div>
- 
-  <div class="text-center">
-    
-    <ul>
-      <button>
-        <li>Lodowka</li>
-      </button>
+      <a class="items-center pl-2.5 mb-5">
+         <span class="self-center text-xl text-center font-semibold whitespace-nowrap cursor-default text-gray-400">
+          <p>Aplikacja wspierajaca<br>prace projektantow</p>  
+        </span>
+      </a>
       
-      <li>Naroznik</li>
+      <hr class="h-1 bg-gray-300 rounded border-0 ">
+
+      <div class="text-center text-xl font-semibold cursor-default text-gray-400 ">
+            <p class="hover:bg-slate-500 rounded-full p-4">
+               Kuchnia
+            </p>
+            
+            <hr class="h-1 bg-gray-300 rounded border-0">
+            
+            <p class="hover:bg-slate-500 rounded-full p-4">
+               Salon
+            </p>
+      </div>
+
+
+      <div class="py-4 px-3rounded flex flex-col text-center text-gray-400 text-xl">
+         
+         <p>
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
+             </button>
+            Lodówka
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
+             </button>
+         </p>
       
-      <li>Zlew</li>
-    
-    </ul>
-  </div>
-  
-</section>
+      </div>
+   </div>
+   
+   
+
 </template>
 
 <style>

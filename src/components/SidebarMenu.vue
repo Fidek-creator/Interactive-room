9b4eb23fb7216d295faca8fa9dc439f6c1d6e5f2
Fidek-creator <fidek1999@gmail.com>
2023-01-03 16:02:53 +0100

    Sidebar funcionality

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -67,16 +67,14 @@
       class="text-center text-xl font-semibold text-gray-400">
       <p
         v-show="selectedRoom == '' || selectedRoom == room.name"
-        @click="selectedRoom = room.name"
-
-
+        @click="room.name == selectedRoom ? selectedRoom='': selectedRoom= room.name"
         class="hover:bg-blue-700 rounded-full p-4 hover:text-gray-300 hover:scale-75 cursor-pointer">
-        {{ room.name }}
+        {{ room.name == selectedRoom ? "Wstecz": room.name }}
       </p>
       <hr class="h-1 bg-gray-300 rounded border-0" />
     </div>
 
-SL: {{ selectedRoom }}
+
 
 
 

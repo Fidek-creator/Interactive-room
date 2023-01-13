bb3b471eea578404035d5ecc253770bae9f46b4b
Fidek-creator <fidek1999@gmail.com>
2023-01-13 19:52:02 +0100

    Added functionality and materials

--- src/components/RangeProp.vue
+++ src/components/RangeProp.vue
@@ -20,14 +20,5 @@
       :max="props.propDefinition.max"
       v-model="props.propDefinition.value"
       class="w-full h-2 bg-gray-300 rounded-lg cursor-pointer" />
-      
-      <label for="default-range" class="block mt-2 mb-2 text-sm font-medium text-gray-400">{{props.propDefinition.name}}</label>
-      <input
-        :id="props.IdPrefix + props.propDefinition.name"
-        type="range"
-        :min="props.propDefinition.min"
-        :max="props.propDefinition.max"
-        v-model="props.propDefinition.value"
-        class="w-full h-2 bg-gray-300 rounded-lg cursor-pointer" />
   </section>
 </template>

1586d09422b33c2c773c311e615abb72249ab6fa
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-15 16:42:32 +0100

    format

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -14,19 +14,14 @@
   });
 </script>
 <template>
-  
   <section v-show="modelValue == '' || modelValue == room.name">
-    
     <p
-    
       :id="props.room.name == props.modelValue ? 'back' : props.room.name.toString().toLowerCase()"
       class="hover:bg-blue-700 rounded-full p-4 hover:text-gray-300 hover:scale-75 cursor-pointer"
       @click="emit('update:modelValue', room.name == modelValue ? '' : room.name)">
       {{ props.room.name == modelValue ? 'Wstecz' : props.room.name }}
-      
     </p>
 
-    
     <Furniture
       v-show="props.room.name == modelValue"
       v-for="furniture in props.room.furnitures"
@@ -35,6 +30,5 @@
       :room="props.room"
       v-model="props.modelValue" />
     <hr class="h-1 bg-gray-300 rounded border-0" />
-      
   </section>
 </template>

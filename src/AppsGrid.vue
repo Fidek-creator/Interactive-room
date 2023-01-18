6e9a6d899bbbba8548497d0cda44c4e1cb41f8e7
Simon Hryszko <simno@hryszko.dev>
2023-01-18 16:30:58 +0100

    Fix overflow

--- src/AppsGrid.vue
+++ src/AppsGrid.vue
@@ -5,7 +5,7 @@ import SidebarMenu from './components/SidebarMenu.vue';
 
 <template>
   <div>
-    <SidebarMenu class="absolute my-auto h-screen rounded-r-lg left-0 top-0 z-10 w-1/6"/>
+    <SidebarMenu class="overflow-y-auto absolute my-auto h-screen rounded-r-lg left-0 top-0 z-10 w-1/6"/>
     <V3DApp class="absolute z-0 inset-0 h-screen w-screen"/>
   </div>
 </template>
